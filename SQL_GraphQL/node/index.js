51e4fd03c39978089bbcf36fa5e0c778965aaff9
Stuart Greenlief <mr.stubob@gmail.com>
2018-10-15 17:59:23 -0600

    mongodb WIP

--- SQL_GraphQL/node/index.js
+++ SQL_GraphQL/node/index.js
@@ -1,21 +1,17 @@
 //Load express module with `require` directive
 var express = require('express');
 var app = express();
-var mysql = require('mysql');
+var MongoClient = require('mongodb').MongoClient;
 
 //Define request response in root URL (/)
 app.get('/', function (req, res) {
     console.log('process.env:', process.env);
-    var connection = mysql.createConnection({
-        host: process.env.MYSQL_HOST,
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD
-    });
-    connection.connect(function (err) {
-        if (err) {
-            res.send('Could not connect to MySQL ' + err.stack);
+    MongoClient.connect(MongoUrl, function(err, db) {
+        if (err !== null) {
+            res.send('Could not connect to MongoDB');
         } else {
-            res.send('Connected to MySQL - Thread ' + connection.threadId);
+            res.send('Connected to MongoDB');
+            db.close();
         }
     });
 });
