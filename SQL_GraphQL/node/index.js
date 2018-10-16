5296ad4e4fdbdd3d427062cf9be049eadf16db6b
root <root@ryz.localdomain>
2018-10-16 01:02:37 -0600

    mongodb working

--- SQL_GraphQL/node/index.js
+++ SQL_GraphQL/node/index.js
@@ -1,24 +1,17 @@
-//Load express module with `require` directive
 var express = require('express');
 var app = express();
-var mysql = require('mysql');
-//Define request response in root URL (/)
-app.get('/', function (req, res) {
-    console.log('process.env:', process.env);
-    var connection = mysql.createConnection({
-        host: process.env.MYSQL_HOST,
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD
-    });
-    connection.connect(function (err) {
-        if (err) {
-            res.send('Could not connect to MySQL ' + err.stack);
+var MongoClient = require('mongodb').MongoClient;
+var MongoUrl = 'mongodb://mongo:27017/nodock';
+
+app.get('/', function(req, res) {
+    MongoClient.connect(MongoUrl, function(err, db) {
+        if (err !== null) {
+            res.send('Could not connect to MongoDB');
         } else {
-            res.send('Connected to MySQL - Thread ' + connection.threadId);
+            res.send('Connected to MongoDB');
+            db.close();
         }
     });
 });
-//Launch listening server on port 8081
-app.listen(8081, function () {
-    console.log('app listening on port 8081!');
-});
+
+app.listen(8081);
